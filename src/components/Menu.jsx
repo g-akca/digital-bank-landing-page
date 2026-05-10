9534f6b6d1275667d177472be688591eb4caf963
g-akca <gok.akca66@gmail.com>
2026-05-10 20:20:54 +0300

    Add shadow behind Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,9 @@
 function Menu() {
   return (
-    <div className="absolute top-22 left-0 w-full px-6">
-      <nav className="py-8 bg-white rounded-sm flex flex-col items-center gap-4 text-blue-950 text-[18px] leading-[155%] tracking-[-0.25px]">
+    <div className="absolute top-16 left-0 w-full p-6">
+      <div className="absolute inset-0 top-0 h-150.75 bg-linear-180 from-blue-950 to-[rgba(0,0,0,0)]" />
+
+      <nav className="relative z-50 py-8 bg-white rounded-sm flex flex-col items-center gap-4 text-blue-950 text-[18px] leading-[155%] tracking-[-0.25px]">
         <a href="#">Home</a>
         <a href="#">About</a>
         <a href="#">Contact</a>
