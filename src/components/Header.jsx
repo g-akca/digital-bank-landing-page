7947c2ee24a0226e2f60174274e8ea893b5fdb36
g-akca <gok.akca66@gmail.com>
2026-05-10 13:25:35 +0300

    Add Header styles and content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import darkLogo from "/images/logo-dark.svg";
+import hamburgerIcon from "/images/icon-hamburger.svg";
+
 function Header() {
   return (
-    <>
-    </>
+    <header className="fixed h-16 w-full p-6 flex justify-between items-center">
+      <img src={darkLogo} alt="Digitalbank logo" className="w-38.5" />
+      <img src={hamburgerIcon} alt="Menu icon" className="w-6" />
+    </header>
   )
 }
 
