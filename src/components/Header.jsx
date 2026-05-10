b39238e2089f1292a0b6bfca50356d7f22fba02f
g-akca <gok.akca66@gmail.com>
2026-05-10 13:34:52 +0300

    Move Header to front and give it white bg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import hamburgerIcon from "/images/icon-hamburger.svg";
 
 function Header() {
   return (
-    <header className="fixed h-16 w-full p-6 flex justify-between items-center">
+    <header className="bg-white fixed z-50 h-16 w-full p-6 flex justify-between items-center">
       <img src={darkLogo} alt="Digitalbank logo" className="w-38.5" />
       <img src={hamburgerIcon} alt="Menu icon" className="w-6" />
     </header>
