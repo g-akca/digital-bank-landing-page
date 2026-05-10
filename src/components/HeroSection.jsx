15edda69bfc8f45f424fc0f822588b6f021bae7f
g-akca <gok.akca66@gmail.com>
2026-05-10 13:34:11 +0300

    Add HeroSection styles and images, create HeroContent

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,15 @@
+import HeroContent from "./HeroContent";
+import introBg from "/images/bg-intro-mobile.svg";
+import mockupsImg from "/images/image-mockups.png";
+
 function HeroSection() {
   return (
-    <>
-    </>
+    <section className="pb-14 flex flex-col gap-12">
+      <img src={introBg} alt="" aria-hidden />
+      <img src={mockupsImg} alt="" aria-hidden className="absolute -top-15 w-[92%] left-1/2 -translate-x-1/2" />
+
+      <HeroContent />
+    </section>
   )
 }
 
