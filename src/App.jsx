f49c75e6dc31e06cf28b891efaffd8060246f4e0
g-akca <gok.akca66@gmail.com>
2026-05-10 15:51:33 +0300

    Add partial Footer content and styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="text-base leading-base text-gray-600 bg-gray-50 min-h-screen">
+    <div className="text-base leading-base text-gray-600 bg-gray-50 min-h-screen flex flex-col">
       <Header />
       <HeroSection />
       <MainSection />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
+import lightLogo from "/images/logo-light.svg";
+import facebookIcon from "/images/icon-facebook.svg";
+import youtubeIcon from "/images/icon-youtube.svg";
+import twitterIcon from "/images/icon-twitter.svg";
+import pinterestIcon from "/images/icon-pinterest.svg";
+import instagramIcon from "/images/icon-instagram.svg";
+
 function Footer() {
   return (
-    <>
-    </>
+    <footer className="grow bg-blue-950 py-14 flex flex-col gap-8 items-center">
+      <div className="flex flex-col gap-8 items-center">
+        <img src={lightLogo} alt="Digitalbank logo" className="w-38.25" />
+
+        <nav className="flex gap-4 items-center">
+          <img src={facebookIcon} alt="Facebook icon" />
+          <img src={youtubeIcon} alt="Youtube icon" />
+          <img src={twitterIcon} alt="Twitter icon" />
+          <img src={pinterestIcon} alt="Pinterest icon" />
+          <img src={instagramIcon} alt="Instagram icon" />
+        </nav>
+      </div>
+
+      <nav>
+
+      </nav>
+
+      <div>
+
+      </div>
+    </footer>
   )
 }
 
