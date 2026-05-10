a06017ae4a16f3619fbb489b946ee22bfaf1f766
g-akca <gok.akca66@gmail.com>
2026-05-10 13:59:57 +0300

    Add negative bottom margin to intro img

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ import mockupsImg from "/images/image-mockups.png";
 function HeroSection() {
   return (
     <section className="pb-14 flex flex-col gap-12">
-      <img src={introBg} alt="" aria-hidden />
+      <img src={introBg} alt="" aria-hidden className="-mb-20" />
       <img src={mockupsImg} alt="" aria-hidden className="absolute -top-15 w-[92%] left-1/2 -translate-x-1/2" />
 
       <HeroContent />
