9442446a81358483648b8701a6e06e4c10c8d9d5
g-akca <gok.akca66@gmail.com>
2026-05-10 20:09:53 +0300

    Add Menu content and styles

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,14 @@
 function Menu() {
   return (
-    <>
-    </>
+    <div className="absolute top-22 left-0 w-full px-6">
+      <nav className="py-8 bg-white rounded-sm flex flex-col items-center gap-4 text-blue-950 text-[18px] leading-[155%] tracking-[-0.25px]">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Blog</a>
+        <a href="#">Careers</a>
+      </nav>
+    </div>
   )
 }
 
