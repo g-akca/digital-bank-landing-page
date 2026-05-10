31acf2565cdab8a37f6170bc8a34bf79a25e71a7
g-akca <gok.akca66@gmail.com>
2026-05-10 22:23:56 +0300

    Add tablet responsive HeroSection and HeroContent styles

--- src/components/HeroContent.jsx
+++ src/components/HeroContent.jsx
@@ -2,10 +2,10 @@ import InviteButton from "./InviteButton";
 
 function HeroContent() {
   return (
-    <div className="self-center max-w-111.75 px-6 flex flex-col gap-6 items-center text-center">
-      <h1 className="text-blue-950 font-light text-[40px] leading-[120%] tracking-[-0.75px]">Next generation digital banking</h1>
+    <div className="self-center max-w-111.5 px-6 tablet:px-0 flex flex-col gap-6 items-center text-center">
+      <h1 className="text-blue-950 font-light text-[40px] tablet:text-[56px] leading-[120%] tracking-[-0.75px] tablet:tracking-[-1px]">Next generation digital banking</h1>
 
-      <p>Take your financial life online. Your Digitalbank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.</p>
+      <p className="tablet:text-[18px] tablet:leading-[155%] tablet:tracking-[-0.25px]">Take your financial life online. Your Digitalbank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.</p>
 
       <InviteButton />
     </div>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,17 @@
 import HeroContent from "./HeroContent";
-import introBg from "/images/bg-intro-mobile.svg";
+import mobileIntroBg from "/images/bg-intro-mobile.svg";
+import desktopIntroBg from "/images/bg-intro-desktop.svg";
 import mockupsImg from "/images/image-mockups.png";
 
 function HeroSection() {
   return (
-    <section className="pb-14 flex flex-col gap-12">
-      <img src={introBg} alt="" aria-hidden className="-mb-20" />
-      <img src={mockupsImg} alt="" aria-hidden className="absolute -top-15 w-[92%] left-1/2 -translate-x-1/2" />
+    <section className="pb-14 tablet:pb-18 flex flex-col gap-12">
+      <picture className="overflow-hidden">
+        <source media="(min-width:768px)" srcset={desktopIntroBg} />
+        <img src={mobileIntroBg} alt="" aria-hidden className="-mb-20 w-full max-w-none tablet:-mb-65 tablet:w-[136%] tablet:relative tablet:-translate-y-68 tablet:left-1/2 tablet:-translate-x-4/9" />
+      </picture>
+
+      <img src={mockupsImg} alt="" aria-hidden className="absolute -top-15 tablet:-top-42.5 w-[92%] tablet:w-[84%] left-1/2 -translate-x-1/2" />
 
       <HeroContent />
     </section>
