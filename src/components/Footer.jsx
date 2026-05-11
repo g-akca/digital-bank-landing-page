7295896bd275a0401c05171e2c39c944d0d603cf
g-akca <gok.akca66@gmail.com>
2026-05-11 20:09:57 +0300

    Add desktop responsive Footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,32 +8,36 @@ import InviteButton from "./InviteButton";
 
 function Footer() {
   return (
-    <footer className="grow bg-blue-950 py-14 flex flex-col gap-8 items-center text-white">
-      <div className="flex flex-col gap-8 items-center">
-        <img src={lightLogo} alt="Digitalbank logo" className="w-38.25" />
+    <footer className="grow bg-blue-950 py-14 desktop:py-12 flex justify-center text-white">
+      <div className="grow max-w-277.5 flex flex-col gap-8 items-center desktop:flex-row desktop:items-start desktop:justify-between">
+        <div className="flex flex-col gap-8 items-center desktop:flex-row desktop:gap-30">
+          <div className="flex flex-col gap-8 items-center desktop:items-stretch desktop:gap-14">
+            <img src={lightLogo} alt="Digitalbank logo" className="w-38.25" />
 
-        <nav className="flex gap-4 items-center">
-          <img src={facebookIcon} alt="Facebook icon" />
-          <img src={youtubeIcon} alt="Youtube icon" />
-          <img src={twitterIcon} alt="Twitter icon" />
-          <img src={pinterestIcon} alt="Pinterest icon" />
-          <img src={instagramIcon} alt="Instagram icon" />
-        </nav>
-      </div>
+            <nav className="flex gap-4 items-center">
+              <img src={facebookIcon} alt="Facebook icon" />
+              <img src={youtubeIcon} alt="Youtube icon" />
+              <img src={twitterIcon} alt="Twitter icon" />
+              <img src={pinterestIcon} alt="Pinterest icon" />
+              <img src={instagramIcon} alt="Instagram icon" />
+            </nav>
+          </div>
 
-      <nav className="flex flex-col gap-4 text-center">
-        <a href="#">About Us</a>
-        <a href="#">Contact</a>
-        <a href="#">Blog</a>
-        <a href="#">Careers</a>
-        <a href="#">Support</a>
-        <a href="#">Privacy Policy</a>
-      </nav>
+          <nav className="flex flex-col gap-4 text-center desktop:text-start desktop:grid desktop:grid-cols-[160px_160px] desktop:gap-y-6 desktop:gap-x-8">
+            <a href="#" className="desktop:row-start-1 desktop:col-start-1">About Us</a>
+            <a href="#" className="desktop:row-start-2 desktop:col-start-1">Contact</a>
+            <a href="#" className="desktop:row-start-3 desktop:col-start-1">Blog</a>
+            <a href="#" className="desktop:row-start-1 desktop:col-start-2">Careers</a>
+            <a href="#" className="desktop:row-start-2 desktop:col-start-2">Support</a>
+            <a href="#" className="desktop:row-start-3 desktop:col-start-2">Privacy Policy</a>
+          </nav>
+        </div>
 
-      <div className="flex flex-col gap-6 items-center">
-        <InviteButton />
+        <div className="flex flex-col gap-6 items-center desktop:items-end">
+          <InviteButton />
 
-        <p className="opacity-50">© Digitalbank. All Rights Reserved</p>
+          <p className="opacity-50">© Digitalbank. All Rights Reserved</p>
+        </div>
       </div>
     </footer>
   )
