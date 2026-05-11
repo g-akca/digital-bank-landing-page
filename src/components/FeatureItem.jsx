1d2c55b11118a9d6037e138068e71b0c94829abb
g-akca <gok.akca66@gmail.com>
2026-05-11 15:40:06 +0300

    Add tablet responsive Features styles

--- src/components/FeatureItem.jsx
+++ src/components/FeatureItem.jsx
@@ -1,11 +1,11 @@
 function FeatureItem({ icon, name, description }) {
   return (
-    <li className="flex flex-col items-center gap-10">
+    <li className="flex flex-col items-center tablet:items-start gap-10">
       <img src={icon} alt={name} />
 
       <div className="flex flex-col gap-6">
         <h3 className="font-light text-blue-950 text-[24px] leading-base tracking-[-0.4px]">{name}</h3>
-        <p>{description}</p>
+        <p className="tablet:text-[16px] tablet:leading-6.5 tablet:tracking-[-0.25px]">{description}</p>
       </div>
     </li>
   )
--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -2,11 +2,11 @@ import FeaturesList from "./FeaturesList";
 
 function FeaturesSection() {
   return (
-    <section className="bg-gray-100 px-6 py-16 flex flex-col gap-12 text-center">
-      <div className="flex flex-col gap-4">
-        <h2 className="font-light text-[32px] leading-[120%] text-blue-950">Why choose Digitalbank?</h2>
+    <section className="bg-gray-100 px-6 py-16 tablet:p-20 flex flex-col gap-12 items-center tablet:gap-18">
+      <div className="flex flex-col gap-4 tablet:gap-8 text-center max-w-129.5">
+        <h2 className="font-light text-[32px] leading-[120%] text-blue-950 tablet:text-[40px] tablet:tracking-[-0.75px]">Why choose Digitalbank?</h2>
 
-        <p>We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.</p>
+        <p className="tablet:text-[18px] tablet:leading-[155%] tablet:tracking-[-0.25px]">We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.</p>
       </div>
 
       <FeaturesList />
