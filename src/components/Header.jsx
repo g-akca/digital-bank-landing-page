1c004828ed2d7ebc61efd40d4691d8a1cfa0ad04
g-akca <gok.akca66@gmail.com>
2026-05-11 16:13:09 +0300

    Adjust max widths

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,20 +8,22 @@ function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white fixed z-50 h-16 w-full p-6 tablet:px-20 flex justify-between items-center">
+    <header className="bg-white fixed z-50 h-16 w-full p-6 tablet:px-20 flex justify-center items-center">
       {isMenuOpen && <Menu />}
 
-      <img src={darkLogo} alt="Digitalbank logo" className="w-38.5" />
+      <div className="w-full flex justify-between items-center max-w-277.5">
+        <img src={darkLogo} alt="Digitalbank logo" className="w-38.5" />
 
-      {isMenuOpen ? (
-        <button type="button" onClick={() => setIsMenuOpen(false)}>
-          <img src={closeIcon} alt="Close menu" className="w-4.5" />
-        </button>
-      ) : (
-        <button type="button" onClick={() => setIsMenuOpen(true)}>
-          <img src={hamburgerIcon} alt="Open menu" className="w-6" />
-        </button>
-      )}
+        {isMenuOpen ? (
+          <button type="button" onClick={() => setIsMenuOpen(false)}>
+            <img src={closeIcon} alt="Close menu" className="w-4.5" />
+          </button>
+        ) : (
+          <button type="button" onClick={() => setIsMenuOpen(true)}>
+            <img src={hamburgerIcon} alt="Open menu" className="w-6" />
+          </button>
+        )}
+      </div>
     </header>
   )
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,10 +8,10 @@ function HeroSection() {
     <section className="pb-14 tablet:pb-18 flex flex-col gap-12">
       <picture className="overflow-hidden">
         <source media="(min-width:768px)" srcSet={desktopIntroBg} />
-        <img src={mobileIntroBg} alt="" aria-hidden className="-mb-20 w-full max-w-none tablet:-mb-68 tablet:w-[137%] tablet:relative tablet:-translate-y-70 tablet:left-1/2 tablet:-translate-x-4/9" />
+        <img src={mobileIntroBg} alt="" aria-hidden className="-mb-20 w-full max-w-300 tablet:-mb-68 tablet:w-[137%] tablet:relative tablet:-translate-y-70 tablet:left-1/2 tablet:-translate-x-4/9" />
       </picture>
 
-      <img src={mockupsImg} alt="" aria-hidden className="absolute -top-15 tablet:-top-42.5 w-[92%] tablet:w-[83%] left-1/2 -translate-x-1/2" />
+      <img src={mockupsImg} alt="" aria-hidden className="absolute -top-15 tablet:-top-42.5 w-[92%] tablet:w-[83%] max-w-185 left-1/2 -translate-x-1/2" />
 
       <HeroContent />
     </section>
