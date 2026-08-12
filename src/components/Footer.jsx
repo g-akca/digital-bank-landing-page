a0e6e848d1c4f371ad1a468c9678d38581d9286a
g-akca <gok.akca66@gmail.com>
2026-08-12 19:24:59 +0300

    Update Footer component to use anchor tags for social media links and add aria-labels for accessibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,26 +14,26 @@ function Footer() {
           <div className="flex flex-col gap-8 items-center desktop:items-stretch desktop:gap-14">
             <img src={lightLogo} alt="Digitalbank logo" className="w-38.25" />
 
-            <nav className="flex gap-4 items-center">
-              <button type="button" className="cursor-pointer">
+            <nav aria-label="Social media" className="flex gap-4 items-center">
+              <a href="#" aria-label="Facebook" className="cursor-pointer">
                 <img src={facebookIcon} alt="Facebook icon" />
-              </button>
-              <button type="button" className="cursor-pointer">
+              </a>
+              <a href="#" aria-label="Youtube" className="cursor-pointer">
                 <img src={youtubeIcon} alt="Youtube icon" />
-              </button>
-              <button type="button" className="cursor-pointer">
+              </a>
+              <a href="#" aria-label="Twitter" className="cursor-pointer">
                 <img src={twitterIcon} alt="Twitter icon" />
-              </button>
-              <button type="button" className="cursor-pointer">
+              </a>
+              <a href="#" aria-label="Pinterest" className="cursor-pointer">
                 <img src={pinterestIcon} alt="Pinterest icon" />
-              </button>
-              <button type="button" className="cursor-pointer">
+              </a>
+              <a href="#" aria-label="Instagram" className="cursor-pointer">
                 <img src={instagramIcon} alt="Instagram icon" />
-              </button>
+              </a>
             </nav>
           </div>
 
-          <nav className="flex flex-col gap-4 text-center desktop:text-start desktop:grid desktop:grid-cols-[160px_160px] desktop:gap-y-6 desktop:gap-x-8">
+          <nav aria-label="Footer" className="flex flex-col gap-4 text-center desktop:text-start desktop:grid desktop:grid-cols-[160px_160px] desktop:gap-y-6 desktop:gap-x-8">
             <a href="#" className="transition-all hover:text-green-400 desktop:row-start-1 desktop:col-start-1">About Us</a>
             <a href="#" className="transition-all hover:text-green-400 desktop:row-start-2 desktop:col-start-1">Contact</a>
             <a href="#" className="transition-all hover:text-green-400 desktop:row-start-3 desktop:col-start-1">Blog</a>
